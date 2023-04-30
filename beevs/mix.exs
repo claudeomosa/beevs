d90982b8f16a74a258a44eaf19aa3c9994c075ec
claudeomosa <claudeomosa@gmail.com>
2023-04-30 14:08:51 +0300

    build(deps):  add credo and sobelow credo - a static code analysis tool sobelow  - a security-focused static analysis tool

--- beevs/mix.exs
+++ beevs/mix.exs
@@ -52,7 +52,9 @@ defmodule Beevs.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:mnemonic_slugs, "~> 0.0.3"}
+      {:mnemonic_slugs, "~> 0.0.3"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false}
     ]
   end
 
