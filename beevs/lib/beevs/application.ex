27550dc322a7f75f402e90345c750c4f41e885f6
claudeomosa <claudeomosa@gmail.com>
2023-05-02 04:23:09 +0300

    generate presence with mix phx.gen.presence this behaviours provides Presence tracking to processes and channels.

--- beevs/lib/beevs/application.ex
+++ beevs/lib/beevs/application.ex
@@ -14,6 +14,8 @@ defmodule Beevs.Application do
       Beevs.Repo,
       # Start the PubSub system
       {Phoenix.PubSub, name: Beevs.PubSub},
+      # Start the Presence tracker
+      BeevsWeb.Presence,
       # Start Finch
       {Finch, name: Beevs.Finch},
       # Start the Endpoint (http/https)
---
+++ beevs/lib/beevs_web/channels/presence.ex
@@ -0,0 +1,11 @@
+defmodule BeevsWeb.Presence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence,
+    otp_app: :beevs,
+    pubsub_server: Beevs.PubSub
+end
