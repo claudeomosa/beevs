24b89a3f12b2f0a82588f1cc689bcc19cda91064
claudeomosa <claudeomosa@gmail.com>
2023-05-02 04:25:16 +0300

    use presence to track online/active users

--- beevs/lib/beevs_web/live/project_live/chat.ex
+++ beevs/lib/beevs_web/live/project_live/chat.ex
@@ -1,11 +1,17 @@
 defmodule BeevsWeb.ProjectLive.Chat do
   use BeevsWeb, :live_view
 
+  require Logger
+
   @impl true
   def mount(params, _session, socket) do
     chat_room_project = Beevs.WorkSpaces.get_project!(params["id"])
-    topic = "chat:#{chat_room_project.id}"
-    if connected?(socket), do: BeevsWeb.Endpoint.subscribe(topic)
+    topic = "chat#{chat_room_project.id}"
+
+    if connected?(socket) do
+      BeevsWeb.Endpoint.subscribe(topic)
+      BeevsWeb.Presence.track(self(), topic, socket.assigns.current_user.id, %{})
+    end
 
     message_changeset = Beevs.WorkSpaces.change_message(%Beevs.WorkSpaces.Message{})
 
@@ -14,8 +20,10 @@ defmodule BeevsWeb.ProjectLive.Chat do
      |> assign(:chat_room_project, chat_room_project)
      |> assign(:message_form, to_form(message_changeset))
      |> assign(:page_title, "Chatroom")
+     |> assign(:active_userlist, [])
      |> assign(:current_user, socket.assigns.current_user)
-     |> assign(:topic, topic)}
+     |> assign(:topic, topic)
+     |> IO.inspect()}
   end
 
   @impl true
@@ -71,6 +79,32 @@ defmodule BeevsWeb.ProjectLive.Chat do
        :chat_room_project,
        Beevs.WorkSpaces.get_project!(socket.assigns.chat_room_project.id)
      )}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    _join_message =
+      joins
+      |> Map.keys()
+      |> Enum.map(fn user_id ->
+        "#{Beevs.Accounts.get_user!(user_id).username}" <> " joined"
+      end)
+
+    _leave_message =
+      leaves
+      |> Map.keys()
+      |> Enum.map(fn user_id ->
+        "#{Beevs.Accounts.get_user!(user_id).username}" <> " left"
+      end)
+
+    active_userlist =
+      BeevsWeb.Presence.list(socket.assigns.topic)
+      |> Map.keys()
+      |> Enum.map(fn user_id ->
+        Beevs.Accounts.get_user!(user_id)
+      end)
+
+    {:noreply, socket |> assign(:active_userlist, active_userlist)}
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
