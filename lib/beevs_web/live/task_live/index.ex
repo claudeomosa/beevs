b390c785126ca7c5d5acad4944fe28aa9318e9c4
claudeomosa <claudeomosa@gmail.com>
2023-05-28 12:44:14 +0300

    add tasks page

---
+++ lib/beevs_web/live/task_live/index.ex
@@ -0,0 +1,13 @@
+defmodule BeevsWeb.TaskLive.Index do
+  alias Beevs.WorkSpaces
+  use BeevsWeb, :live_view
+  @impl true
+  def mount(_params, _session, socket) do
+    my_tasks =  Enum.filter(WorkSpaces.list_tasks(), fn task ->
+      task.user_id == socket.assigns.current_user.id
+    end )
+    IO.inspect(my_tasks)
+    {:ok, socket |> assign(:my_tasks, my_tasks)}
+  end
+
+end
